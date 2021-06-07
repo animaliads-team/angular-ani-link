ae08eb061f221746d6aae9f88a30c2d003238205
animaliads-team <ds.animalia@gmail.com>
2021-06-07 09:43:56 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ani-link
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ani-link)
---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import '@animaliads/ani-link';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {}
